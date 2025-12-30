4bf05b1bd46dd153fd2034ff039426f29803ca15
youseffathy <yf1102003@gmail.com>
2025-12-30 14:29:29 +0200

    fix: update service worker revision and migrate scripts to use bun

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.u5phtpqjf2"
+    "revision": "0.edrfp6chbcc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
